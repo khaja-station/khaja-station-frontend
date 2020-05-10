510f7201bd6d56b9ab84128e82e573bb9e1dab10
shrijan <shrijan00003@gmail.com>
2020-05-11 02:25:08 +0545

    master layout renamed as men-dahboard-layout

---
+++ src/layouts/menu-dashboard.layout.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+interface Props {}
+const MenuDashboardLayout: React.FC<Props> = () => {
+  return <div className="menu-dashboard-wrapper"></div>;
+};
+export default MenuDashboardLayout;
