6372a88633d6b63b1532c7cdfc0a571f792842e3
shrijan <shrijan00003@gmail.com>
2020-05-16 03:09:27 +0545

    - i18n is fixed with the backend plugin and placed in public folder

--- src/app/app.i18n.ts
+++ src/app/app.i18n.ts
@@ -1,16 +1,14 @@
 import i18n from 'i18next';
-import { resources } from 'lang/index';
+import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: 'en',
-  fallbackLng: 'en',
-  interpolation: {
-    escapeValue: false,
-  },
-  react: {
-    useSuspense: false,
-    wait: true,
-  },
-});
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
