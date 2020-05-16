11cd9cafb36590741a32b2601209290e76d0ed25
shrijan <shrijan00003@gmail.com>
2020-05-16 23:59:23 +0545

    reverted to previous setup for i18n

--- src/app/app.i18n.ts
+++ src/app/app.i18n.ts
@@ -1,14 +1,12 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
+import { resources } from 'lang';
 
-i18n
-  .use(Backend)
-  .use(initReactI18next)
-  .init({
-    lng: 'en',
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
---
+++ src/lang/index.ts
@@ -0,0 +1,8 @@
+import commonEnglish from './en/common.json';
+export const resources = {
+  en: {
+    translation: {
+      common: { ...commonEnglish },
+    },
+  },
+};
