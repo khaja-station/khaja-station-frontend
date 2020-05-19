f72fa4aece98d2c6960eb6c34844436f61d6472d
shrijan <shrijan00003@gmail.com>
2020-05-19 13:51:22 +0545

    yup validation for password added

--- src/auth/auth.validation.ts
+++ src/auth/auth.validation.ts
@@ -5,8 +5,13 @@ import * as Yup from 'yup';
 const INVALID_EMAIL = 'Email is invalid';
 const REQUIRED_FIELD = 'Field is required';
 const INVALID_PASSWORD_LENGTH = 'Invalid password length';
+const PASSWORD_ERROR =
+  'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character';
 
 export const loginValidationSchema = Yup.object({
   email: Yup.string().email(INVALID_EMAIL).required(REQUIRED_FIELD),
-  password: Yup.string().min(8, INVALID_PASSWORD_LENGTH).required(REQUIRED_FIELD),
+  password: Yup.string()
+    .min(8, INVALID_PASSWORD_LENGTH)
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/, PASSWORD_ERROR)
+    .required(REQUIRED_FIELD),
 });
